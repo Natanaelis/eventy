f6fe8c938adf0d372e0ddfa09dd58580a30ab1db
Wojciech Kubala <60062709+Natanaelis@users.noreply.github.com>
2021-05-16 14:07:40 +0200

    Initial commit

---
+++ components/event/EventAdd.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form ref="form">
+    <v-text-field v-model="title" label="Title" required></v-text-field>
+    <v-text-field v-model="id" label="Id" required></v-text-field>
+    <v-text-field v-model="date" label="Date" required></v-text-field>
+    <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+
+    <v-btn color="warning" @click="send"> Send </v-btn>
+  </v-form>
+</template>
+<script>
+export default {
+  data: () => ({
+    title: '',
+    id: '',
+    date: '',
+  }),
+
+  methods: {
+    reset() {
+      this.$refs.form.reset()
+    },
+    send() {
+      console.log(this.title)
+    },
+  },
+}
+</script>
