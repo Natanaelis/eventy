f2eec3363decd86c828d95244729c99f8c899158
Tomasz Kryń <tf.kryn@gmail.com>
2021-06-12 09:21:52 +0200

    Add main search component

---
+++ components/search/SearchMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row class="hero-form-row">
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="cinty"
+            :rules="cintyRules"
+            label="Miasto"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="date"
+            :rules="dateRules"
+            label="Data"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-btn color="primary" class="align-self-baseline">Szukaj</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    cinty: '',
+    date: '',
+    cintyRules: [(v) => !!v || 'Podanie nazwy miasta jest wymagane'],
+    dateRules: [(v) => !!v || 'Podanie daty jest wymagane'],
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+.hero-form-row {
+  align-items: baseline;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,13 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
+        <h1>Wyszukaj eventy!</h1>
+        <SearchMain />
       </div>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
-}
+export default {}
 </script>
