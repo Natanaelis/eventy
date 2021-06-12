cb04f594979d73bf1bd070c2d8741ee7bd3adda4
Tomasz Kryń <tf.kryn@gmail.com>
2021-06-12 10:51:25 +0200

    Add filters

--- components/search/SearchMain.vue
+++ components/search/SearchMain.vue
@@ -1,26 +1,57 @@
 <template>
-  <v-form v-model="valid">
+  <v-form v-model="valid" @submit.prevent="search">
     <v-container>
       <v-row class="hero-form-row">
         <v-col cols="12" md="4">
           <v-text-field
-            v-model="cinty"
-            :rules="cintyRules"
+            v-model="city"
+            :rules="cityRules"
             label="Miasto"
             required
           ></v-text-field>
         </v-col>
 
         <v-col cols="12" md="4">
-          <v-text-field
-            v-model="date"
-            :rules="dateRules"
-            label="Data"
-            required
-          ></v-text-field>
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                readonly
+                prepend-icon="mdi-calendar"
+                :rules="dateRules"
+                label="Data"
+                required
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              locale="pl-pl"
+              :first-day-of-week="1"
+              no-title
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="menu = false"> Anuluj </v-btn>
+              <v-btn text color="primary" @click="$refs.menu.save(date)">
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
         </v-col>
         <v-col>
-          <v-btn color="primary" class="align-self-baseline">Szukaj</v-btn>
+          <v-btn color="primary" class="align-self-baseline" type="submit">
+            Szukaj
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -31,11 +62,25 @@
 export default {
   data: () => ({
     valid: false,
-    cinty: '',
-    date: '',
-    cintyRules: [(v) => !!v || 'Podanie nazwy miasta jest wymagane'],
+    date: new Date().toISOString().substr(0, 10),
+    menu: false,
+    city: '',
+    cityRules: [(v) => !!v || 'Podanie nazwy miasta jest wymagane'],
     dateRules: [(v) => !!v || 'Podanie daty jest wymagane'],
   }),
+  methods: {
+    search() {
+      if (this.valid) {
+        this.$router.push({
+          path: 'event',
+          query: {
+            city: this.city,
+            date: this.date,
+          },
+        })
+      }
+    },
+  },
 }
 </script>
 
--- pages/event/index.vue
+++ pages/event/index.vue
@@ -1,23 +1,50 @@
 <template>
   <v-row>
     <v-col class="text-center">
+      <h1 v-if="city && date">
+        Wyniki wyszukiwania dla miasta: {{ city }} w dniu {{ date }}
+      </h1>
       <EventItem
-        v-for="item in items"
+        v-for="item in filteredItems"
         :key="item.id"
         :title="item.title"
         :item="item"
       />
+      <v-alert
+        v-if="!filteredItems.length"
+        border="right"
+        colored-border
+        type="error"
+        elevation="2"
+        class="mt-6"
+      >
+        Brak wyników
+      </v-alert>
     </v-col>
   </v-row>
 </template>
 <script>
 import { mapGetters } from 'vuex'
 export default {
-  data: () => ({}),
   computed: {
     ...mapGetters({
       items: 'event/items',
     }),
+    city() {
+      return this.$route.query.city
+    },
+    date() {
+      return this.$route.query.date
+    },
+    filteredItems() {
+      if (this.date) {
+        const filtered = this.items.filter((item) => {
+          return item.date === this.date
+        })
+        return filtered
+      }
+      return this.items
+    },
   },
 }
 </script>
